2c68cf9cfb70428ec40c5408c48322f7cba8b4fa
shruthi2306 <shruthi.srinivasan01@sjsu.edu>
2022-02-10 22:13:31 -0800

    Okta authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+//import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+import Home from '../src/pages/Home'
+import Details from './pages/Details';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <Route path="/" exact={true} component={Home} />
+    {/* <SecureRoute path="/details" component={Details}/>
+   */}
+    </Router>
     </div>
   );
 }
---
+++ src/pages/Details.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+//import { withAuth } from '@okta/okta-react';
+
+export default function Details (){
+    return(
+        <div>Home</div>
+    )
+
+}
